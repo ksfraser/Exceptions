e7721454a5a5b9557ae85df14d53a0d04a232fe1
Kevin Fraser <kevin@ksfraser.ca>
2026-02-10 14:08:43 -0700

    Test scaffolding + coverage improvements

---
+++ tests/DocsOnlyTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Ksfraser\Exceptions\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class DocsOnlyTest extends TestCase
+{
+    public function testPackageLoads(): void
+    {
+        $this->assertTrue(true);
+    }
+}
