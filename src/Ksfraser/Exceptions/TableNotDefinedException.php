8635e23eabfbd9f0329e9c2ebaae16bbf7f3d73f
Kevin Fraser <kevin@ksfraser.ca>
2026-02-16 13:54:33 -0700

    Add custom KSF exception classes

---
+++ src/Ksfraser/Exceptions/TableNotDefinedException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ksfraser\Exceptions;
+
+class TableNotDefinedException extends CustomException
+{
+}
---
+++ src/Ksfraser/Exceptions/VarNotSetException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ksfraser\Exceptions;
+
+class VarNotSetException extends CustomException
+{
+}
